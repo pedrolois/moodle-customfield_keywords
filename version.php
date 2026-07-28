33bfa10946b92225fe483bbd97e50d5ac4a550e6
Pedro Luis <pgarcia@unicc.org>
2026-07-28 23:15:09 +0200

    Bump version build number for Moodle Marketplace resubmission

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'customfield_keywords';
-$plugin->version   = 2026072100;
+$plugin->version   = 2026072201;
 $plugin->requires  = 2024100100;
-$plugin->release   = '1.0.0';
+$plugin->release   = '1.0.1';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->supported = [405, 405];
