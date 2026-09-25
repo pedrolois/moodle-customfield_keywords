6e40b30f684456bed232b2928b7c3c10386743b4
Pedro Luis Garcia Leyva <pgarcia@unicc.org>
2026-09-25 14:21:45 +0200

    Release 1.0.8: tests, CI and rebuilt AMD

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'customfield_keywords';
-$plugin->version   = 2026092500;
+$plugin->version   = 2026092501;
 $plugin->requires  = 2024100100;
-$plugin->release   = '1.0.7';
+$plugin->release   = '1.0.8';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->supported = [405, 405];
